b2873effea42db70136a8d2dab31ca323b059a03
Shreyas-Bh <ishaanbht@yahoo.com>
2026-04-16 23:06:09 +0530

    treasure_hunt.py

--- treasure_hunt.py
+++ treasure_hunt.py
@@ -24,7 +24,7 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 print("You are at a cross road. Where do you want to go?")
 
-diredtion = input("Type \"left\" or \"right\".\n").lower()
+direction = input("Type \"left\" or \"right\".\n").lower()
 if direction == "left":
     print("You have reached the shore. Do you want to wait for a boat or swim?")
     wait = input("Type \"wait\" to wait for the boat. Type \"swim\" to swim across.\n").lower()
